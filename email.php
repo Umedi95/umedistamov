43023eed918e330afa2ffb61ba166e7633ce27ef
Umed Istamov <umed.istamov@contentcreation.com.my>
2019-09-26 17:19:07 +0800

    email

--- email.php
+++ email.php
@@ -1,10 +1,9 @@
 <?php
-if($_POST){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-//send email
-    mail("istamov9595@gmail.com", "This is an email from:" .$email, $message);
-}
+    $toEmail = "istamov9595@gmail.com";
+    $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
+    if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) {
+        print "<p class='success'>Mail Sent.</p>";
+    } else {
+        print "<p class='Error'>Problem in Sending Mail.</p>";
+    }
 ?>
